095f018b12f4f8f869b890261ff719d15efbdde3
Ikievy <88610456+W3e-Wee@users.noreply.github.com>
2022-07-08 14:16:32 +0800

    Allows Routing to each screen #3

--- react-app-example/src/App.js
+++ react-app-example/src/App.js
@@ -5,9 +5,24 @@ import './App.css';
 import NavBar from './component/NavBar';
 import FooterBar from './component/FooterBar';
 
-// Screen
-import NoScreen from './screen/NoScreen';
+// Screens
 import HomeScreen from './screen/HomeScreen';
+import ProductsScreen from './screen/ProductScreen';
+import NoScreen from './screen/NoScreen';
+import SingleProductScreen from './screen/SingleProductScreen';
+
+import {
+  Route,
+  useParams,
+  Routes,
+  BrowserRouter
+} from "react-router-dom";
+
+
+const ProductIdWrapper = () => {
+  const { productId } = useParams();
+  return <SingleProductScreen productId={productId} />;
+};
 
 class App extends React.Component {
   constructor() {
@@ -18,7 +33,16 @@ class App extends React.Component {
     return (
       <div>
         <NavBar />
-        <h1>Hello React!</h1>
+
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<HomeScreen />} />
+            <Route path="/products" element={<ProductsScreen />} />
+            <Route path="/products/:productId" element={<ProductIdWrapper />} />
+            <Route path="*" element={<NoScreen />} />
+          </Routes>
+        </BrowserRouter>
+
         <FooterBar />
       </div>
     );
